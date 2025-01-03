23df582a4f5f1188f350c698d3314751475eed24
mizan <mizanurrahmanmizan934@gmail.com>
2025-01-03 23:03:46 +0600

    expanding cards file create :star:

---
+++ Top Up/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.view-receipt-btn').addEventListener('click', () => {
+    alert('Viewing receipt...');
+  });
+  
+  document.querySelector('.cancel-btn').addEventListener('click', () => {
+    alert('Cancelled.');
+  });
+  
