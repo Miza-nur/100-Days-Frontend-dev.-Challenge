2f48b6f2b8726632c450769015d9483217be2e2c
mizan <mizanurrahmanmizan934@gmail.com>
2025-03-01 22:32:26 +0600

    :star:Delete & Update btn

--- To_do_List/script.js
+++ To_do_List/script.js
@@ -1,17 +1,36 @@
-const addclick=()=>{
-    const task=document.getElementById('task');
-    const taskinput=task.value.trim();
+const addclick = () => {
+    const task = document.getElementById('task');
+    const taskinput = task.value.trim();
 
-    if(taskinput !== ''){
-        const tasklist=document.getElementById('tasklist');
-        const li=document.createElement('li')
-        li.innerHTML=`
-        <input type="checkbox"> 
-        `+ taskinput +`<i class="fa-solid fa-plus"></i>
-        <i class="fa-solid fa-trash"></i>`;
+    if (taskinput !== '') {
+        const tasklist = document.getElementById('tasklist');
+        const li = document.createElement('li');
+
+        li.innerHTML = `
+            <input type="checkbox"> 
+            ${taskinput}
+            <button onclick="editTask(this)"> <i class="fa-solid fa-pen-to-square"></i></button> 
+            <button onclick="deleteTask(this)"> <i class="fa-solid fa-trash"></i></button>
+        `;
+
         tasklist.appendChild(li);
-
-    }else{
+        task.value = '';
+    } else {
         alert("Please input TO DO ");
     }
-}
+};
+
+const deleteTask = (btn) => {
+    const li = btn.parentNode;
+    li.parentNode.removeChild(li);
+};
+
+const editTask = (btn) => {
+    const li = btn.parentNode;
+    const text = li.childNodes[2].textContent.trim();
+    const newText = prompt("Edit your task:", text);
+
+    if (newText !== null && newText.trim() !== '') {
+        li.childNodes[2].textContent = " " + newText + " ";
+    }
+};
