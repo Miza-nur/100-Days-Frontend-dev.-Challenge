f894232241b117ef628fa9e7c59eb2035a34dd6e
mizan <mizanurrahmanmizan934@gmail.com>
2025-03-03 22:59:26 +0600

    :star:

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,16 +1,27 @@
 const getWeather=()=>{
     const city=document.getElementById('cityName').value;
     const apikey='a49c71d1481e5cac637dee217388e860';
-    const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+    const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
 
     fetch(apiUrl)
     .then(res=>res.json())
     .then(data =>{
-        console.log(data)
+        
+        const weatherInfo=document.getElementById('weatherInfo');
 
         const description=data.weather[0].description;
         const temperature=data.main.temp;
         const humidity=data.main.humidity;
+        const wind=data.wind.speed;
+
+        weatherInfo.innerHTML=`
+        <p>Description : ${description}</p>
+        <p>Temperature : ${temperature} &#x2103</p>
+        
+        <p>Humidity : ${humidity} &#x0025</p>
+        <p>wind Speed : ${wind} m/s</p>
+        `
+
     
     
     })
